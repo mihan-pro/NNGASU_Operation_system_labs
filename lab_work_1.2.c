8467f02f0f927891eb6e080394577856addb1ed2
mihan-pro <mihan-pro@mail.ru>
2021-01-09 16:28:32 +0300

    add lab work 1.2

--- lab_work_1.2.c
+++ lab_work_1.2.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define BUFFER_SIZE 256
 
 int main() {
+    char buffer[BUFFER_SIZE];
+    FILE* file = fopen("textFile.txt", "r");
+    if(file == NULL) 
+    {
+        perror("we have some problem with opening file\n");
+        exit(1);
+    }
+    while (fgets(buffer, BUFFER_SIZE, file))
+    {
+        printf("%s", buffer);
+    }    
+    printf("\n");
+    fclose(file);
     return 0;
 }
