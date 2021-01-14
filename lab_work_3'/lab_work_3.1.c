104303d7a626282e0d0e1cb3fa69f428017b5d8b
mihan-pro <mihan-pro@mail.ru>
2021-01-14 19:44:49 +0300

    lab work 3.3 has been done

--- lab_work_3'/lab_work_3.1.c
+++ lab_work_3'/lab_work_3.1.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    int result = system("./../lab_work_2/lw2.2");
+    int i = 0;
+    time_t startTime = time(NULL);
+    while(i++ < 10000)
+    {
+        int result = system("./../lab_work_2/lw2.2");
+    }
+    time_t endTime =  time(NULL);
+    printf("%ld", (endTime - startTime));
+    printf("\n i'm a parent process Thanks for your attention \n");
     return 0;
 }
+
+/*
+    It takes 18 seconds to execute this code inside while loop.
+*/
--- lab_work_3'/lab_work_3.2.c
+++ lab_work_3'/lab_work_3.2.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 int main()
 {
-    pid_t result = fork();
-    if (result == -1)
+    int i = 0;
+    time_t startTime = time(NULL);
+    while(i++ < 10000)
     {
-        perror("we couldn't start new process");
-        exit(1);
+        pid_t result = fork();
+        if (result == -1)
+        {
+            perror("we couldn't start new process");
+            exit(1);
+        }
+        else if(result == 0) 
+        {
+            char* args[] = {"lw2.2", NULL};
+            execv("./../lab_work_2/lw2.2", args);
+        } 
     }
-    else if(result == 0) 
-    {
-        char* args[] = {"lw2.2", NULL};
-        execv("./../lab_work_2/lw2.2", args);
-    }
-    printf("i'm a parent process Thanks for your attention \n");
+    time_t endTime =  time(NULL);
+    printf("%ld", (endTime - startTime));
+    printf("\n i'm a parent process Thanks for your attention \n");
     return 0;
 }
+
+/*
+    It takes 1-2 seconds to execute this code inside while loop.
+*/
